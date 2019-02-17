ca2e19517689820782d77bdbcf673679972f67c4
Mark Zsombor <markszsombor@gmail.com>
2019-02-17 12:41:19 -0800

    quick cleanup

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Uter
+# Create-React-Snake
 
-This is an update to my 2017 Battlesnake to the new 2018 engine.
+This is my 2019 Battlesnake, based on my old snake.
 
 A [BattleSnake AI](https://battlesnake.io) written in Javascript using the pathfinding module.
 
--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(function (err, req, res, next) {
   res.status(404)
   res.send({
     status: 404,
-    error: err.message || "This is the home of Uter the snake. Check out battlesnake.io to find out more"
+    error: err.message || "This is the home of my snake. Check out battlesnake.io to find out more"
   })
 
   return
