aac7ab4af6ec92c40b883c730bab1c4327270524
Mark Zsombor <markszsombor@gmail.com>
2019-02-18 22:28:48 -0800

    added checks for if food is a dead end

--- routes/helpers.js
+++ routes/helpers.js
@@ -57,18 +57,19 @@ function findFood(gameState) {
     return a.distance - b.distance;
   });
   //Return the closest
-  return allTargets[0];
+  return { target: allTargets[0], type: "food" };
 }
 
 // Finds your own tail and returns its coordinates for targeting.
 function findTail(gameState) {
-  let snakeBody = gameState.you.body;
-  let snakeLength = snakeBody.length;
+  let { body } = gameState.you;
+  let snakeLength = body.length;
   if (snakeLength === 1) {
     return findFood(gameState);
   }
-  let tailPosition = snakeBody[snakeLength - 1];
-  return tailPosition;
+  let tailPosition = body[snakeLength - 1];
+  console.log("tail", tailPosition);
+  return { target: tailPosition, type: "tail" };
 }
 
 // Checks current health to switch between tail chasing and food chasing.
@@ -78,8 +79,10 @@ function chooseTarget(gameState, grid) {
     (snakes.length == 2 && gameState.you.health > 60) ||
     !gameState.board.food.length
   ) {
+    console.log("get tail");
     return findTail(gameState);
   } else {
+    console.log("get food");
     return findFood(gameState);
   }
 }
@@ -88,13 +91,23 @@ function chooseTarget(gameState, grid) {
 function checkSelf(gameState, possibleMoves) {
   const { body } = gameState.you;
   for (let i = 0; i < body.length - 1; i++) {
-    for (let move in possibleMoves) {
+    for (let move of possibleMoves) {
+      if (move.valid && move.x === body[i].x && move.y === body[i].y) {
+        move.valid = false;
+      }
+    }
+  }
+  if (
+    body[body.length - 1].x == body[body.length - 2].x &&
+    body[body.length - 1].y == body[body.length - 2].y
+  ) {
+    for (let move of possibleMoves) {
       if (
-        possibleMoves[move].valid &&
-        possibleMoves[move].x === body[i].x &&
-        possibleMoves[move].y === body[i].y
+        move.valid &&
+        move.x === body[body.length - 1].x &&
+        move.y === body[body.length - 1].y
       ) {
-        possibleMoves[move].valid = false;
+        move.valid = false;
       }
     }
   }
@@ -103,18 +116,12 @@ function checkSelf(gameState, possibleMoves) {
 
 //Stop from running into wall
 function checkEdges(gameState, possibleMoves) {
-  for (let move in possibleMoves) {
-    if (
-      possibleMoves[move].x < 0 ||
-      possibleMoves[move].x >= gameState.board.width
-    ) {
-      possibleMoves[move].valid = false;
+  for (let move of possibleMoves) {
+    if (move.x < 0 || move.x >= gameState.board.width) {
+      move.valid = false;
     }
-    if (
-      possibleMoves[move].y < 0 ||
-      possibleMoves[move].y >= gameState.board.height
-    ) {
-      possibleMoves[move].valid = false;
+    if (move.y < 0 || move.y >= gameState.board.height) {
+      move.valid = false;
     }
   }
   return possibleMoves;
@@ -127,13 +134,13 @@ function checkSnakes(gameState, possibleMoves) {
     if (snakes[snake].id !== gameState.you.id) {
       //Don't run into body
       for (let i = 0; i < snakes[snake].body.length - 1; i++) {
-        for (let move in possibleMoves) {
+        for (let move of possibleMoves) {
           if (
-            possibleMoves[move].valid &&
-            possibleMoves[move].x === snakes[snake].body[i].x &&
-            possibleMoves[move].y === snakes[snake].body[i].y
+            move.valid &&
+            move.x === snakes[snake].body[i].x &&
+            move.y === snakes[snake].body[i].y
           ) {
-            possibleMoves[move].valid = false;
+            move.valid = false;
           }
         }
       }
@@ -170,85 +177,82 @@ function checkOtherHeads(gameState, possibleMoves) {
 }
 
 //Marks areas on the Grid where the snake can't pass into
-function setGrid(gameState) {
+function setGrid(gameState, targetType) {
   const { you, board } = gameState;
-  const myHead = you.body[0];
+  const { body } = you;
+  const myHead = body[0];
   const { snakes } = board;
   const grid = new PF.Grid(board.width, board.height);
 
   //Mark my snake in grid
-  for (let i = 1; i < you.body.length - 1; i++) {
-    grid.setWalkableAt(you.body[i].x, you.body[i].y, false);
+  for (let i = 1; i < body.length - 1; i++) {
+    console.log(body[i].x, body[i].y, false);
+    grid.setWalkableAt(body[i].x, body[i].y, false);
+  }
+
+  //Don't mark my tail -1 if just ate but won't collide
+  if (
+    targetType == "tail" ||
+    (body[body.length - 1].x == body[body.length - 2].x &&
+      body[body.length - 1].y == body[body.length - 2].y &&
+      getDistance(body[body.length - 1].x, body[body.length - 1].y, myHead) > 1)
+  ) {
+    grid.setWalkableAt(body[body.length - 1].x, body[body.length - 1].y, true);
   }
 
   //Mark other snake heads
-  for (let snake in snakes) {
-    if (snakes[snake].id !== you.id) {
+  for (let snake of snakes) {
+    if (snake.id !== you.id) {
       //Don't run into body
 
-      for (let j = 0; j < snakes[snake].body.length; j++) {
-        grid.setWalkableAt(
-          snakes[snake].body[j].x,
-          snakes[snake].body[j].y,
-          false
-        );
+      for (let j = 0; j < snake.body.length; j++) {
+        console.log(snake.body[j].x, snake.body[j].y, false);
+        grid.setWalkableAt(snake.body[j].x, snake.body[j].y, false);
       }
       //Could we run into the head this turn
-      if (
-        getDistance(
-          snakes[snake].body[0].x,
-          snakes[snake].body[0].y,
-          myHead
-        ) === 2
-      ) {
+      if (getDistance(snake.body[0].x, snake.body[0].y, myHead) === 2) {
         //Decide on head collision depending on size
-        if (you.body.length <= snakes[snake].body.length) {
+        if (body.length <= snake.body.length) {
           //Pathfinding will throw an error if we try to set a space outside the board
-          if (snakes[snake].body[0].x + 1 < board.width) {
-            grid.setWalkableAt(
-              snakes[snake].body[0].x + 1,
-              snakes[snake].body[0].y,
-              false
-            );
+          if (snake.body[0].x + 1 < board.width) {
+            console.log(snake.body[0].x + 1, snake.body[0].y, false);
+            grid.setWalkableAt(snake.body[0].x + 1, snake.body[0].y, false);
           }
-          if (snakes[snake].body[0].x - 1 >= 0) {
-            grid.setWalkableAt(
-              snakes[snake].body[0].x - 1,
-              snakes[snake].body[0].y,
-              false
-            );
+          if (snake.body[0].x - 1 >= 0) {
+            console.log(snake.body[0].x - 1, snake.body[0].y, false);
+            grid.setWalkableAt(snake.body[0].x - 1, snake.body[0].y, false);
           }
-          if (snakes[snake].body[0].y + 1 < board.height) {
-            grid.setWalkableAt(
-              snakes[snake].body[0].x,
-              snakes[snake].body[0].y + 1,
-              false
-            );
+          if (snake.body[0].y + 1 < board.height) {
+            console.log(snake.body[0].x, snake.body[0].y + 1, false);
+            grid.setWalkableAt(snake.body[0].x, snake.body[0].y + 1, false);
           }
-          if (snakes[snake].body[0].y - 1 >= 0) {
-            grid.setWalkableAt(
-              snakes[snake].body[0].x,
-              snakes[snake].body[0].y - 1,
-              false
-            );
+          if (snake.body[0].y - 1 >= 0) {
+            console.log(snake.body[0].x, snake.body[0].y - 1, false);
+            grid.setWalkableAt(snake.body[0].x, snake.body[0].y - 1, false);
           }
         }
       }
     }
   }
+  console.log(JSON.stringify(grid));
+
   return grid;
 }
 
-function generatePath(gameState, target) {
+function generatePath(gameState, grid, target) {
   const { you, board } = gameState;
   const myHead = you.body[0];
-
   // Set the board, choose the target and generate a path
   try {
-    const grid = setGrid(gameState);
     const finder = new PF.AStarFinder();
 
-    const path = finder.findPath(myHead.x, myHead.y, target.x, target.y, grid);
+    const path = finder.findPath(
+      myHead.x,
+      myHead.y,
+      target.target.x,
+      target.target.y,
+      grid
+    );
     return path;
   } catch (e) {
     console.error(e);
@@ -259,6 +263,7 @@ function noPathFallback(gameState) {
   console.log("NO PATH");
   const { you, board } = gameState;
   const myHead = you.body[0];
+  console.log(myHead);
   let possibleMoves = [
     {
       direction: "right",
@@ -306,7 +311,29 @@ function noPathFallback(gameState) {
     validMoves = possibleMoves.filter(move => move.valid);
   }
   const random = Math.floor(Math.random() * validMoves.length);
+  console.log(possibleMoves);
   return validMoves[random].direction;
+}
+
+function validatePath(gameState, path) {
+  const { you, board } = gameState;
+  path = path
+    .map(move => {
+      return { x: move[0], y: move[1] };
+    })
+    .reverse();
+  path.pop();
+  const newBody = path.concat(you.body);
+  newBody.length = you.body.length + 1;
+  const newGameState = { ...gameState, you: { ...you, body: newBody } };
+  const grid = setGrid(newGameState, "tail");
+
+  try {
+    const newPath = generatePath(newGameState, grid, findTail(newGameState));
+    return newPath.length;
+  } catch (e) {
+    return false;
+  }
 }
 
 module.exports = {
@@ -320,5 +347,7 @@ module.exports = {
   findFood,
   getDistance,
   setMove,
-  noPathFallback
+  noPathFallback,
+  validatePath,
+  setGrid
 };
--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,10 @@ const {
   checkEdges,
   checkSelf,
   setMove,
-  noPathFallback
+  noPathFallback,
+  findTail,
+  validatePath,
+  setGrid
 } = require("./helpers");
 
 app.use(bodyParser.json());
@@ -35,17 +38,37 @@ router.post("/move", function(req, res) {
   const gameState = req.body;
   const { you, board } = gameState;
   const myHead = you.body[0];
-
-  const path = generatePath(gameState, chooseTarget(gameState));
   const snakeResponse = {};
+  let validPath = true;
+  try {
+    const grid = setGrid(gameState);
+    const gridBackup = grid.clone();
+    let path = generatePath(gameState, grid, chooseTarget(gameState));
+    if (path.length) {
+      console.log("initial path", path.length);
+      validPath = validatePath(gameState, path);
+    }
+    if (!validPath) {
+      const newGrid = setGrid(req.body)
+      console.log("switch to tail");
+      path = generatePath(gameState, newGrid, findTail(gameState));
+    }
 
-  // if no path exists choose a safe direction
-  if (!path.length) {
+    // if no path exists choose a safe direction
+    if (!path.length || !validPath) {
+      // console.log(JSON.stringify(gameState));
+      snakeResponse.move = noPathFallback(gameState);
+      console.log(snakeResponse);
+      return res.json(snakeResponse);
+    } else {
+      snakeResponse.move = setMove(path, myHead);
+      console.log(snakeResponse);
+      return res.json(snakeResponse);
+    }
+  } catch (e) {
+    console.log(JSON.stringify(gameState));
+    console.log(e);
     snakeResponse.move = noPathFallback(gameState);
-    console.log(snakeResponse);
-    return res.json(snakeResponse);
-  } else {
-    snakeResponse.move = setMove(path, myHead);
     console.log(snakeResponse);
     return res.json(snakeResponse);
   }
